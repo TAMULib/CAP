22183c19b69f9e074b6e85c3e336819d442c3ffb
jeremythuff <jeremythuff@gmail.com>
2017-12-06 15:18:51 -0600

    Init commit

---
+++ src/main/java/edu/tamu/cap/CapApplication.java
@@ -0,0 +1,37 @@
+package edu.tamu.cap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "edu.tamu.*" })
+public class CapApplication extends SpringBootServletInitializer {
+	
+	/**
+     * Entry point to the application from within servlet.
+     *
+     * @param args
+     *            String[]
+     *
+     */
+	public static void main(String[] args) {
+		SpringApplication.run(CapApplication.class, args);
+	}
+	
+	/**
+     * Entry point to the application if run using spring-boot:run.
+     *
+     * @param application
+     *            SpringApplicationBuilder
+     *
+     * @return SpringApplicationBuilder
+     *
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CapApplication.class);
+    }
+}
