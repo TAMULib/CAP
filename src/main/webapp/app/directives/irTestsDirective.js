31bfb0cb886ffbcd0ccb91f8768fee1c6b7538a6
Jeremy Huff <jeremy@full-165-91-49-102.vpn.tamu.edu>
2017-12-19 14:54:16 -0600

    Added files

---
+++ src/main/webapp/app/directives/irTestsDirective.js
@@ -0,0 +1,62 @@
+cap.directive("irtests", function(IRRepo, $q) {
+    return {
+        templateUrl: "views/directives/irTests.html",
+        restrict: "E",
+        scope: {
+            ir: "="
+        },
+        link: function($scope, attr, elem) {
+            $scope.testIrConnection = function() {
+
+            $scope.testFinished = false;
+
+            $scope.irTests = [
+              {
+                name: "Pinging "+$scope.ir.uri,
+                key: "testingPing",
+                execute: IRRepo.testPing,
+                status: "PENDING"
+              }
+            ]
+        
+            if($scope.ir.username && $scope.ir.password) {
+              $scope.irTests.push({
+                name: "Testing Authentication",
+                key: "testingAuth",
+                execute: IRRepo.testAuth,
+                status: "PENDING"
+              });
+            }
+        
+            $scope.irTests.push({
+              name: "Retrieving Top Level Content",
+              key: "testingContent",
+              execute: IRRepo.testContent,
+              status: "PENDING"
+            });
+
+            var chain = $q.when();
+            angular.forEach($scope.irTests, function (test) {
+              chain = chain.then(function() {
+                return test.execute($scope.ir).then(function(res) {
+                  test.status = angular.fromJson(res.body).meta.status;
+                });
+              });
+            });
+            chain.then(function() {
+              var status = "SUCCESS";
+              for(var i in $scope.irTests) {
+                var test = $scope.irTests[i];
+                if(test.status!==status) {
+                  status=test.status;
+                  break;
+                }
+              };
+              $scope.testFinished = status;
+            });
+            return chain;
+            
+          };
+        }
+    }
+});
