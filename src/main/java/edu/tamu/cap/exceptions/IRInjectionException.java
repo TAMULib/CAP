8c87bb0e1f009b78bb908b7bb2df59ac8c85861b
jeremythuff <jeremythuff@gmail.com>
2017-12-21 13:02:07 -0600

    Added missing files

---
+++ src/main/java/edu/tamu/cap/exceptions/IRInjectionException.java
@@ -0,0 +1,14 @@
+package edu.tamu.cap.exceptions;
+
+public class IRInjectionException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -787392244167335114L;
+	
+	public IRInjectionException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/edu/tamu/cap/exceptions/IRVerificationException.java
@@ -0,0 +1,14 @@
+package edu.tamu.cap.exceptions;
+
+public class IRVerificationException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1846189124129780195L;
+	
+	public IRVerificationException(String message) {
+		super(message);
+	}
+	
+}
