89e425a0d4fb6185b5586f498a62f02690911008
Jeremy Huff <jeremy@full-165-91-49-222.vpn.tamu.edu>
2017-12-22 01:08:56 -0600

    Improved breadcrumb filter

--- src/main/webapp/app/filters/breadcrumbFilter.js
+++ src/main/webapp/app/filters/breadcrumbFilter.js
@@ -1,13 +1,18 @@
+
 cap.filter("breadcrumb", function() {
+
+    var lastValue = null;
+
     return function(input,ir) {
         var output = input;
         if(input && ir) {
             if(output === ir.rootUri) {
                 output = "Root"
             } else {
-                output = output.replace(ir.rootUri, "");
-            }
+                output = input.replace(lastValue, "...")  ;
+            } 
         }
+        lastValue = input;
         return output;
     }
 });
