a5d45a5a56c31f2c23a18e5bcaac21f69ef2d58e
jeremythuff <jeremythuff@gmail.com>
2018-01-05 17:13:37 -0600

    Using angular.equals in properties filter

--- src/main/webapp/app/filters/mapPropertiesFilter.js
+++ src/main/webapp/app/filters/mapPropertiesFilter.js
@@ -5,15 +5,17 @@ cap.filter("mapProperties", function() {
 
   return function(input) {
     var prepOutput = {};
+
     angular.forEach(input, function(triple) {
       if(!prepOutput[triple.predicate]) prepOutput[triple.predicate] = [];
       prepOutput[triple.predicate].push(triple.object);
     });
 
-    if(angular.toJson(prepOutput)!==angular.toJson(output)) {
+    if(!angular.equals(prepOutput, output)) {
       angular.extend(output, prepOutput);
     }
 
     return output;
   };
+
 });
