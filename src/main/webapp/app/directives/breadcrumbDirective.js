c0cdddb9abfb69d0ae1bf5e14c3ffef0e2e7e06a
William Welling <wwelling@library.tamu.edu>
2018-01-11 07:36:07 -0600

    replace breacrumb name root uri if no title set

--- src/main/webapp/app/directives/breadcrumbDirective.js
+++ src/main/webapp/app/directives/breadcrumbDirective.js
@@ -8,6 +8,14 @@ cap.directive("breadcrumbs", function() {
         link: function($scope, attr, elem) {
             $scope.breadcrumbs = [];
 
+            $scope.trimName = function(context) {
+              var name = context.name;
+              if (context.name.startsWith(context.ir.rootUri)) {
+                name = name.replace(context.ir.rootUri, '...');
+              }
+              return name;
+            };
+
             var getParent = function(context) {
                 var parentContext = $scope.context.ir.getContext(context.parent.object);
                 if(parentContext.parent) {
