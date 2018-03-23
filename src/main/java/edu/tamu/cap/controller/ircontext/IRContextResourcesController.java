e8c128d5c79b5b98eb35e99cf71bd41337ddcbb0
jeremythuff <jeremythuff@gmail.com>
2018-03-23 17:41:19 -0500

    Added missing files

---
+++ src/main/java/edu/tamu/cap/controller/ircontext/IRContextResourcesController.java
@@ -0,0 +1,57 @@
+package edu.tamu.cap.controller.ircontext;
+
+import static edu.tamu.weaver.response.ApiStatus.SUCCESS;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+
+import java.util.Map;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import edu.tamu.cap.model.response.Triple;
+import edu.tamu.cap.service.IRService;
+import edu.tamu.weaver.response.ApiResponse;
+
+@RestController
+@RequestMapping("ir-context/{type}/{irid}/resource")
+public class IRContextResourcesController {
+    
+    @RequestMapping(method = POST)
+    @PreAuthorize("hasRole('USER')")
+    public ApiResponse createResource(IRService<?> irService, @Param("contextUri") String contextUri, @RequestParam("file") MultipartFile file) throws Exception {
+        return new ApiResponse(SUCCESS, irService.createResource(contextUri, file));
+    }
+    
+    @RequestMapping(method = GET)
+    @PreAuthorize("hasRole('USER')")
+    public ApiResponse getResources(IRService<?> irService, @Param("contextUri") String contextUri) throws Exception {
+        return new ApiResponse(SUCCESS, irService.getResource(contextUri));
+    }
+    
+    @RequestMapping(method = PUT)
+    @PreAuthorize("hasRole('USER')")
+    public ApiResponse updateResources(IRService<?> irService, @Param("contextUri") String contextUri, @RequestParam("file") MultipartFile file) throws Exception {
+        return new ApiResponse(SUCCESS, irService.updateResource(contextUri, file));
+    }
+    
+    @RequestMapping(method = DELETE)
+    @PreAuthorize("hasRole('USER')")
+    public ApiResponse deleteResources(IRService<?> irService, @Param("contextUri") String contextUri) throws Exception {
+        irService.deleteResource(contextUri);
+        return new ApiResponse(SUCCESS);
+    }
+
+    @RequestMapping(value = "/fixity", method = GET)
+    @PreAuthorize("hasRole('USER')")
+    public ApiResponse createResource(IRService<?> irService, @RequestParam Map<String, String> tripleMap) throws Exception {
+        return new ApiResponse(SUCCESS, irService.resourceFixity(Triple.of(tripleMap)));
+    }
+
+}
