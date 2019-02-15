e719c9c08a54ed36420ac884360d006c9141a564
jsavell <jsavell@library.tamu.edu>
2019-02-15 13:40:13 -0600

    configurable ui for different auth strategies, configure cap for email reg by default

--- src/main/webapp/app/config/appConfig.js
+++ src/main/webapp/app/config/appConfig.js
@@ -5,6 +5,8 @@ var appConfig = {
     'allowAnonymous': true,
     'anonymousRole': 'ROLE_ANONYMOUS',
 
+    'authStrategies': ['emailRegistration'],
+
     'authService': window.location.protocol + '//' + window.location.host + window.location.base+'/auth',
     'webService': window.location.protocol + '//' + window.location.host + window.location.base,
 
---
+++ src/main/webapp/app/controllers/appLoginController.js
@@ -0,0 +1,22 @@
+cap.controller('AppLoginController', function ($controller, $location, $scope, UserService, StorageService) {
+
+    angular.extend(this, $controller('LoginController', {
+        $scope: $scope
+    }));
+
+    $scope.checkAuthStrategy = function(strategy) {
+        return (appConfig.authStrategies.indexOf(strategy) > -1);
+    };
+
+    $scope.isShibEnabled = function() {
+        return $scope.checkAuthStrategy('shibboleth');
+    };
+
+    $scope.isEmailEnabled = function() {
+        return $scope.checkAuthStrategy('emailRegistration');
+    };
+
+    $scope.getShibUser = function() {
+        UserService.fetchUser();
+    };
+});
