e493e3b057a8d5200afb93cd8c438e9c6541c302
jeremythuff <jeremythuff@gmail.com>
2019-03-13 14:48:49 -0500

    Removed uneeded slash on snippet path

--- src/test/java/edu/tamu/cap/controller/VerifyRepositoryViewSettingsControllerTest.java
+++ src/test/java/edu/tamu/cap/controller/VerifyRepositoryViewSettingsControllerTest.java
@@ -37,7 +37,7 @@ import edu.tamu.cap.utility.ConstraintDescriptionsHelper;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
-//@AutoConfigureRestDocs(outputDir = "/target/generated-snippets")
+@AutoConfigureRestDocs(outputDir = "target/generated-snippets")
 public final class VerifyRepositoryViewSettingsControllerTest {
     private static final String TEST_REPOSITORY_VIEW_TYPE = "repository-view/{type}/verify";
     private static final String TEST_REPOSITORY_VIEW_NAME = "TEST_REPOSITORY_VIEW_NAME";
