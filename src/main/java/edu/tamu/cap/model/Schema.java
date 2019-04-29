7f81f95459931b25e866063fb65c54783df0596d
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2019-04-29 15:04:20 -0500

    Fixed formatting

--- src/main/java/edu/tamu/cap/model/Schema.java
+++ src/main/java/edu/tamu/cap/model/Schema.java
@@ -11,50 +11,50 @@ import edu.tamu.cap.model.validation.SchemaValidator;
 import edu.tamu.weaver.validation.model.ValidatingBaseEntity;
 
 @Entity
-public class Schema extends ValidatingBaseEntity  {
-	
-	@Column
-	private String name;
-	
-	@Column
+public class Schema extends ValidatingBaseEntity {
+
+    @Column
+    private String name;
+
+    @Column
     private String abbreviation;
-	
-	@Column
-	private String namespace;
-	
-	@ElementCollection
-	private List<Property> properties;
-	
-	public Schema() {
-		setModelValidator(new SchemaValidator());
-		setProperties(new ArrayList<Property>());
-	}
-	
-	public Schema(String name, String namespace, String abbreviation, List<Property> properties) {
-		this();
-		setName(name);
-		setNamespace(namespace);
-		setAbbreviation(abbreviation);
-		setProperties(properties);
-	}
-	
-	public Schema(String name, String namespace, String abbreviation) {
-		this();
-		setName(name);
-		setNamespace(namespace);
-		setAbbreviation(abbreviation);
-		setProperties(new ArrayList<Property>());
-	}
 
-	public String getName() {
-		return name;
-	}
+    @Column
+    private String namespace;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    @ElementCollection
+    private List<Property> properties;
 
-	public String getAbbreviation() {
+    public Schema() {
+        setModelValidator(new SchemaValidator());
+        setProperties(new ArrayList<Property>());
+    }
+
+    public Schema(String name, String namespace, String abbreviation, List<Property> properties) {
+        this();
+        setName(name);
+        setNamespace(namespace);
+        setAbbreviation(abbreviation);
+        setProperties(properties);
+    }
+
+    public Schema(String name, String namespace, String abbreviation) {
+        this();
+        setName(name);
+        setNamespace(namespace);
+        setAbbreviation(abbreviation);
+        setProperties(new ArrayList<Property>());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbbreviation() {
         return abbreviation;
     }
 
@@ -63,27 +63,27 @@ public class Schema extends ValidatingBaseEntity  {
     }
 
     public String getNamespace() {
-		return namespace;
-	}
+        return namespace;
+    }
 
-	public void setNamespace(String prefix) {
-		this.namespace = prefix;
-	}
+    public void setNamespace(String prefix) {
+        this.namespace = prefix;
+    }
 
-	public List<Property> getProperties() {
-		return properties;
-	}
+    public List<Property> getProperties() {
+        return properties;
+    }
 
-	public void setProperties(List<Property> properties) {
-		this.properties = properties;
-	}
-	
-	public void addProperty(Property property) {
-		this.properties.add(property);
-	}
-	
-	public void removeProperty(Property property) {
-		this.properties.remove(property);
-	}
-	
+    public void setProperties(List<Property> properties) {
+        this.properties = properties;
+    }
+
+    public void addProperty(Property property) {
+        this.properties.add(property);
+    }
+
+    public void removeProperty(Property property) {
+        this.properties.remove(property);
+    }
+
 }
