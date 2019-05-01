b67a4250fff8ebcd5cc439d14205b277186aee3a
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2019-05-01 10:22:30 -0500

    Added a messaging profile

--- src/main/java/edu/tamu/cap/service/aspect/MessagingAspect.java
+++ src/main/java/edu/tamu/cap/service/aspect/MessagingAspect.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerMapping;
@@ -25,6 +26,7 @@ import edu.tamu.weaver.messaging.service.MessagingService;
 
 @Aspect
 @Component
+@Profile("weaver-messaging")
 @Scope(value = SCOPE_REQUEST)
 public class MessagingAspect {
 
