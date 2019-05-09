e376797b451219fae57571e540f0d4874f557ebe
William Welling <wwelling@library.tamu.edu>
2019-05-09 10:34:28 -0500

    abstract isCollapsed and fixed scope issue

--- src/main/webapp/app/controllers/abstractAppController.js
+++ src/main/webapp/app/controllers/abstractAppController.js
@@ -1,4 +1,5 @@
 cap.controller("AbstractAppController", function($controller, $scope) {
+
   angular.extend(this, $controller('AbstractController', {
         $scope: $scope
   }));
@@ -6,4 +7,18 @@ cap.controller("AbstractAppController", function($controller, $scope) {
   $scope.isCurator = function () {
       return (sessionStorage.role === "ROLE_CURATOR");
   };
+
+  $scope.isCollapsable = function(triples, predicate) {
+    var matches = 0;
+    for(var triple of triples) {
+      if(triple.predicate === predicate) {
+        matches++;
+        if(matches > 1) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
 });
