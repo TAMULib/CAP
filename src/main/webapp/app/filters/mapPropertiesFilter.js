24aecf737f48dff642ccc8e1606cb17c73061048
William Welling <wwelling@library.tamu.edu>
2019-10-04 16:26:34 -0500

    a bit more minor cleanup

--- src/main/webapp/app/filters/mapPropertiesFilter.js
+++ src/main/webapp/app/filters/mapPropertiesFilter.js
@@ -4,17 +4,16 @@ cap.filter("mapProperties", function() {
   var output = {};
 
   return function(input) {
-    
+
     var prepOutput = {};
 
     angular.forEach(input, function(triple) {
-      if(!prepOutput[triple.predicate]) prepOutput[triple.predicate] = [];
+      if (!prepOutput[triple.predicate]) prepOutput[triple.predicate] = [];
       prepOutput[triple.predicate].push(triple.object);
     });
 
-    if(!angular.equals(prepOutput, output)) {
+    if (!angular.equals(prepOutput, output)) {
       angular.extend(output, prepOutput);
-      console.log(output);
     }
 
     return input;
