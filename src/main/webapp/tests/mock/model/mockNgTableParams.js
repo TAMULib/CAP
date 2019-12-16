9cb4a4a9a089459b0e44f914f101e2a2d44191d8
Kevin Day <kday@library.tamu.edu>
2019-12-16 14:46:39 -0600

    Update NgTableParams mock

--- src/main/webapp/tests/mock/model/mockNgTableParams.js
+++ src/main/webapp/tests/mock/model/mockNgTableParams.js
@@ -13,27 +13,19 @@ var dataNgTableParams3 = {
   data: []
 };
 
-var dataNgTableParams4 = {
-  id: 4,
-  data: []
-};
-
-var dataNgTableParams5 = {
-  id: 5,
-  data: []
-};
-
-var dataNgTableParams6 = {
-  id: 6,
-  data: []
-};
-
 var mockNgTableParams = function($q) {
   var model = mockModel("NgTableParams", $q, dataNgTableParams1);
 
+  model._settings = {
+    counts: [],
+    total: 0,
+    getData: function(params) {
+      return model.data;
+    }
+  };
+
   model.count = function() {
-    var total = 0;
-    return total;
+    return model._settings.total;
   };
 
   model.sorting = function(sort) {
